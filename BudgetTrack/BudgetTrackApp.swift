7a0ba07df2dfdfede797ade471975b6349469ab6
Baran Haydar <baranhaydar80@gmail.com>
2026-03-19 16:46:36 +0300

    Initial Commit

---
+++ BudgetTrack/BudgetTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackApp.swift
+//  BudgetTrack
+//
+//  Created by Baran on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
