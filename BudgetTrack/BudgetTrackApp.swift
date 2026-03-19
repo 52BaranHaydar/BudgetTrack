65e313ff0b01d3392433fcc997125057aaeb0264
Baran Haydar <baranhaydar80@gmail.com>
2026-03-19 19:33:25 +0300

    Initial commit - BudgetTrack iOS app

--- BudgetTrack/BudgetTrackApp.swift
+++ BudgetTrack/BudgetTrackApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct BudgetTrackApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
